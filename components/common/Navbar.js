6d8d6a69b022f6d4ad0dad385c44d23913a9f6e1
duchuytran943 <duchuytran943@gmail.com>
2021-02-23 17:28:42 +0700

    UI Navigation Bar

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -1,6 +1,15 @@
+import styles from "styles/components/Navbar.module.scss";
+import Logo from "./Logo";
+import NavbarList from "./NavbarList";
+
 const Navbar = ({ children }) => (
   <>
-    <div>Navbar</div>
+    <nav className={styles.navBarWrap}>
+      <div className={styles.navBar}>
+        <Logo></Logo>
+        <NavbarList></NavbarList>
+      </div>
+    </nav>
   </>
 );
 
