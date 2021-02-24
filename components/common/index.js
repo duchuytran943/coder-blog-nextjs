cf58267a271330964c424e539f1d01da0bb3862d
duchuytran943 <duchuytran943@gmail.com>
2021-02-24 22:11:43 +0700

    Layout main container

--- components/common/index.js
+++ components/common/index.js
@@ -4,6 +4,5 @@ import Footer from "./Footer";
 import Logo from "./Logo";
 import NavbarList from "./NavbarList";
 import CustomLink from "./CustomLink";
-import Banner from "./Banner";
 
-export { Layout, Navbar, Footer, Logo, NavbarList, CustomLink, Banner };
+export { Layout, Navbar, Footer, Logo, NavbarList, CustomLink };
---
+++ components/home/SideContainer.js
@@ -0,0 +1,11 @@
+import styles from "styles/components/SideContainer.module.scss";
+
+const SideContainer = () => {
+  return (
+    <div className={styles.sideContainer}>
+      <div>Tag</div>
+    </div>
+  );
+};
+
+export default SideContainer;
