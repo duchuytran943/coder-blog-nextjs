fb3199d8691ef9d4834a772adca8dc7a6ade6d96
duchuytran943 <duchuytran943@gmail.com>
2021-02-24 17:55:27 +0700

    UI footer

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,6 +1,16 @@
+import { Logo } from "components/common";
+import styles from "styles/components/Footer.module.scss";
+
 const Footer = () => (
   <>
-    <div>Footer</div>
+    <div className={styles.footerWrap}>
+      <div className={styles.footer}>
+        <Logo className={styles.logo}></Logo>
+        <div className={styles.blogInfo}>
+          © 2021. Pet Blog project of mine base on Thinkster.
+        </div>
+      </div>
+    </div>
   </>
 );
 
--- components/common/Logo.js
+++ components/common/Logo.js
@@ -1,10 +1,10 @@
-import styles from "styles/components/Navbar.module.scss";
+import styles from "styles/components/Logo.module.scss";
 import CustomLink from "./CustomLink";
 
-const Logo = () => {
+const Logo = ({ className }) => {
   return (
     <CustomLink href={"/"} as={"/"}>
-      <h1 className={styles.logo}>HuyTran's Blog</h1>
+      <h1 className={className || styles.logo}>HuyTran's Blog</h1>
     </CustomLink>
   );
 };
